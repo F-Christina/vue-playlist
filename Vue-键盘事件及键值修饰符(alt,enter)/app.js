309eaa0df91a9fa37c348d83f95a83e3d6c8aa9f
F-Christina <840208498@qq.com>
2017-12-04 16:33:17 +0800

    Vue-键盘事件及键值修饰符(alt,enter)
    Vue-键盘事件及键值修饰符(alt,enter)

---
+++ Vue-键盘事件及键值修饰符(alt,enter)/app.js
@@ -0,0 +1,19 @@
+//实例化vue对象
+new Vue({
+	el:"#vue-app",
+	data:{
+		
+	},
+	methods:{
+		logName: function (){
+			console.log("你正在输入名字！")
+		},
+		logAge: function (){
+			console.log("你正在输入年龄！")
+		}
+	}
+});
+
+/*
+* 
+*/
