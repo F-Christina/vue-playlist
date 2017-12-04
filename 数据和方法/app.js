088902eb482d3e19a90d62bf88a75bd40a0d8a69
F-Christina <840208498@qq.com>
2017-12-04 15:23:59 +0800

    数据和方法
    数据和方法

---
+++ 数据和方法/app.js
@@ -0,0 +1,18 @@
+//实例化vue对象
+new Vue({
+	el:"#vue-app",
+	data:{
+		name:"Christina",
+		job:"web开发"
+	},
+	methods:{
+		greet: function(time){
+			return 'Good '+ time + " " + this.name +'!'
+		}
+	}
+});
+
+/*
+* el: element需要获取的元素，一定是html中的根容器元素
+* data: 用于数据的存储
+*/
