21bbe50f7be9a5096ed196f623cd637237bd50ee
F-Christina <840208498@qq.com>
2017-12-05 17:38:01 +0800

    路由和http
    路由和http

--- vue-playlist/src/App.vue
+++ vue-playlist/src/App.vue
@@ -1,50 +1,29 @@
 <!-- 1.模板：html解构 -->
 <template>
   <div id="app">
-    <app-header v-on:titleChanged="updateTitle($event)" v-bind:title="title"></app-header>
-    <users v-bind:users="users"></users>
-    <users v-bind:users="users"></users>
-    <app-footer v-bind:title="title"></app-footer>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/helloworld">Hello</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
   </div>
 </template>
 
 <!-- 2.行为：处理逻辑 -->
 <script>
 //局部注册组件
-import Users from './components/Users'
-import Header from './components/Header'
-import Footer from './components/Footer'
+
 
 export default {
   name: 'app',
   data(){
     return {
-      //title: "这是我的第一个Vue脚手架项目！"
-      users:[
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false},
-        {name:"Henry",position:"web开发",show:false}
-      ],
-      title:"传递的是一个值，（number string boolean）"
+      
     }
-  },
-  methods:{
-    updateTitle(title){
-      this.title = title;
-    }
-  },
-  components:{
-    "users":Users,
-    "app-header":Header,
-    "app-footer":Footer
   }
 }
 </script>
