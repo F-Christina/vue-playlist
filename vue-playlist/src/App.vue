cf3818ab6831f04d22dbd5f2243d08b0fd689615
F-Christina <840208498@qq.com>
2017-12-05 14:28:11 +0800

    介绍SEC文件流程及根组件App
    介绍SEC文件流程及根组件App（初始化）

---
+++ vue-playlist/src/App.vue
@@ -0,0 +1,24 @@
+<!-- 1.模板：html解构 -->
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<!-- 2.行为：处理逻辑 -->
+<script>
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      title: "这是我的第一个Vue脚手架项目！"
+    }
+  }
+}
+</script>
+
+<!-- 3.样式：解决样式 -->
+<style>
+
+</style>
